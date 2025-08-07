d77734cbfd113d1ecbcbd943d75c746a08217b5b
yanfeich <yanfei.cheng@intel.com>
2025-08-07 08:51:40 +0000

    fix scale list 0x80 alignment

--- fastdeploy/model_executor/layers/moe/fused_moe_hpu_backend.py
+++ fastdeploy/model_executor/layers/moe/fused_moe_hpu_backend.py
@@ -142,10 +142,11 @@ class HpuTensorWiseFP8MoEMethod(HpuMoEMethod):
         self.quant_fn = paddlenlp_ops.fused_quant
         self.moe_quant_type = "tensor_wise_fp8"
 
-        align_dummp = paddle.zeros([1], dtype=ffn1_weights[0].dtype)
-        align_list = []
-        for j in range(layer.num_local_experts-1):
-            align_list.append(align_dummp)
+        align_dummy = paddle.zeros([1], dtype=ffn1_weights[0].dtype)
+        padding_list = []
+        # align to 0x80 (128 bytes) / 2 (bf16) = 64, add 63 padding tensors
+        for j in range(63):
+            padding_list.append(align_dummy)
 
         for idx, weights_tensor in enumerate([ffn1_weights, ffn2_weights]):
             weights_name = self.added_weight_attrs[idx]
@@ -159,7 +160,7 @@ class HpuTensorWiseFP8MoEMethod(HpuMoEMethod):
                 quant_weight, scale = self.quant_fn(weights_tensor[i])
                 weights_list.append(quant_weight)
                 scales_list.append(scale)
-                scales_list.extend(align_list)
+                scales_list.extend(padding_list)
 
             quanted_weight = paddle.stack(weights_list, axis=0)
             create_and_set_parameter(layer, weights_name, quanted_weight)
